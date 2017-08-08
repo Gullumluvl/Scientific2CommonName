c71216c6fde982ee05c48cca4524e0dc3279a012
glouvel <glouvel@biologie.ens.fr>
2017-08-08 18:30:05 +0200

    handle NA

--- FindClade.py
+++ FindClade.py
@@ -14,7 +14,7 @@ URL = "https://www.ncbi.nlm.nih.gov/taxonomy/"
 REPORT = "info"
 
 
-def main(names, level='family', http=False):
+def main(names, level='family', http=False, na='NA'):
     # Check if names are given as arguments of the script. If not, read stdin.
     # TODO: error when no data provided to stdin after some time lapse.
     url = URL.replace('https', 'http') if http else URL
@@ -29,7 +29,7 @@ def main(names, level='family', http=False):
             raise r.raise_for_status()
         soup = bs4.BeautifulSoup(r.text, 'lxml')
         #print soup.dl
-        out = 'NA'
+        out = na
         for dl in soup.findAll('dl'): # description lists
             if dl.dt.text == "Lineage:": # title of the description list item
                 for a in dl.findAll('a'): # link
@@ -45,9 +45,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-l", "--level", default="family",
                         help="taxonomic level to fetch [%(default)s]")
-    parser.add_argument("names", nargs='*', help="species names to convert, if empty, read from stdin")
+    parser.add_argument("names", nargs='*',
+                        help="species names to convert, if empty, read from stdin")
     parser.add_argument("--http", action='store_true',
                         help="Use http:// instead of https://")
+    parser.add_argument("--NAempty", action='store_const', dest=na,
+                        const='', default='NA',
+                        help="prints the empty string when not found (otherwise %(default)r)")
     args = parser.parse_args()
     main(**vars(args))
 
