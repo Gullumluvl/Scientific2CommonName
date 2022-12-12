5f083209852a193969f263b66da8ae8e83149248
GullumLuvl <7593801+Gullumluvl@users.noreply.github.com>
2022-12-12 11:56:03 +0100

    Query multiple taxonomic ranks at once

--- FindClade.py
+++ FindClade.py
@@ -21,7 +21,7 @@ URL_ASSEMBLY = "https://www.ncbi.nlm.nih.gov/assembly/"
 REPORT = "info"
 
 
-def main(names, level='family', http=False, na='NA', assembly=False):
+def iter_clades(names, levels=['family'], http=False, na='NA', assembly=False):
     # Check if names are given as arguments of the script. If not, read stdin.
     # TODO: error when no data provided to stdin after some time lapse.
     url = URL.replace('https', 'http') if http else URL
@@ -34,22 +34,25 @@ def main(names, level='family', http=False, na='NA', assembly=False):
             print("Reason:", r.reason, file=sys.stderr)
             raise r.raise_for_status()
         soup = bs4.BeautifulSoup(r.text, 'lxml')
-        out = na
+        out = {level: na for level in levels}
+        found_levels = 0
         for dl in soup.findAll('dl'): # description lists
             if dl.dt.text == "Lineage:": # title of the description list item
                 for a in dl.findAll('a'): # link
                     atitle = a.attrs.get('title')
-                    if atitle == level:
-                        out = a.text
-                        break
+                    if atitle in levels:
+                        out[atitle] = a.text
+                        found_levels += 1
+                        if found_levels == len(levels):
+                            break
                 break
-        print(out)
+        yield [out[level] for level in levels]
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-l", "--level", default="family",
-                        help="taxonomic level to fetch [%(default)s]")
+    parser.add_argument("-l", "--levels", default="family",
+                        help="taxonomic levels to fetch, comma-separated [%(default)s]")
     parser.add_argument("names", nargs='*',
                         help="species names to convert, if empty, read from stdin")
     parser.add_argument("--http", action='store_true',
@@ -57,5 +60,11 @@ if __name__ == '__main__':
     parser.add_argument("--NAempty", action='store_const', dest='na',
                         const='', default='NA',
                         help="prints the empty string when not found (otherwise %(default)r)")
-    args = parser.parse_args()
-    main(**vars(args))
+    kwargs = vars(parser.parse_args())
+    kwargs['levels'] = [x.strip() for x in kwargs['levels'].split(',')]
+    for clades in iter_clades(**kwargs):
+        print('\t'.join(clades))
+
+
+if __name__ == '__main__':
+    main()
